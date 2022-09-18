26df6b28ee4910ec640826c2726b4111c7137d11
Dinar <dinar-wp9@mail.ru>
2022-09-18 13:05:57 +0300

    first commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,14 +2,13 @@
 
 class MainClass
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-
-        string myName;
-        myName = "Dinar";
-
-        Console.WriteLine(myName);
-
+        const string MyName = "Dinar";
+        Console.WriteLine(MyName);
+        Console.WriteLine("\n \t Мне 21 год");
+        Console.WriteLine("\n \t lol");
+        Console.WriteLine("\n \t Whats are you doing  \n Dinar");
         Console.ReadKey();
     }
 }
